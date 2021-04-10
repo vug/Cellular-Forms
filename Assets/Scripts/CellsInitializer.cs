a157585d90a77dd6ad60dfb2c04e1002e0ab2e0a
Uğur Güney <ugurguney@gmail.com>
2021-04-10 18:49:36 -0400

    Set link order manually at initialization
    The normal direction depends on the link order.  Permuted linked cells to get correct, outward normal.
    
    I'll leave this tetrahedron soon anyway.

--- Assets/Scripts/CellsInitializer.cs
+++ Assets/Scripts/CellsInitializer.cs
@@ -19,13 +19,21 @@ public class CellsInitializer : MonoBehaviour
     {
         Vector3[] vertices = new[] {
             new Vector3(Mathf.Sqrt(8.0f / 9.0f), 0, -1.0f / 3.0f),
-            new Vector3(- Mathf.Sqrt(2.0f / 9.0f), Mathf.Sqrt(2.0f / 3.0f), -1.0f / 3.0f),
-            new Vector3(-Mathf.Sqrt(2.0f / 9.0f), - Mathf.Sqrt(2.0f / 3.0f), -1.0f / 3.0f),
+            new Vector3(-Mathf.Sqrt(2.0f / 9.0f), Mathf.Sqrt(2.0f / 3.0f), -1.0f / 3.0f),
+            new Vector3(-Mathf.Sqrt(2.0f / 9.0f), -Mathf.Sqrt(2.0f / 3.0f), -1.0f / 3.0f),
             new Vector3(0, 0, 1),
         };
+        var links = new Dictionary<int, int[]>
+        {
+            [0] = new int[] { 1, 3, 2 }, // { 1, 2, 3 } was inwards
+            [1] = new int[] { 2, 3, 0 },
+            [2] = new int[] { 0, 3, 1 }, // { 0, 1, 3 } was inwards
+            [3] = new int[] { 0, 1, 2 },
+        };
+
 
         Cell[] cells = new Cell[4];
-        for(int i=0; i<4; i++)
+        for (int i = 0; i < 4; i++)
         {
             GameObject obj = Instantiate(this.cellPrefab);
             cells[i] = obj.GetComponent<Cell>();
@@ -33,9 +41,8 @@ public class CellsInitializer : MonoBehaviour
         }
         for (int i = 0; i < 4; i++)
         {
-            for (int j = 0; j < 4; j++)
+            foreach (int j in links[i])
             {
-                if (i == j) { continue; }
                 cells[i].links.Add(cells[j]);
             }
         }
