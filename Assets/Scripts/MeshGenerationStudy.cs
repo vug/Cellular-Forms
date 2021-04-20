dcb64409faaf41315028043489b5dbedc2771483
Uğur Güney <ugurguney@gmail.com>
2021-04-20 00:54:37 -0400

    Remove unused MeshGenerationStudy code

--- Assets/Scripts/MeshGenerationStudy.cs
+++ Assets/Scripts/MeshGenerationStudy.cs
@@ -1,16 +1,9 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerationStudy : MonoBehaviour
 {
-    Mesh mesh;
-
-    Vector3[] vertices;
-    int[] triangles;
-
     void Start()
     {
         HalfEdgeMesh heMesh = HalfEdgeMeshGenerator.readHalfEdge("Assets/Meshes/icosahedron.halfedge");
@@ -23,36 +16,5 @@ public class MeshGenerationStudy : MonoBehaviour
             Debug.Log("vertex: " + v);
         }
         Debug.Log("triangles: " + String.Join(",", meshFilter.mesh.triangles));
-
-        //mesh = new Mesh();
-        //GetComponent<MeshFilter>().mesh = mesh;
-        //CreateShape();
-        //UpdateMesh();
-    }
-
-    void CreateShape()
-    {
-        vertices = new Vector3[]
-        {
-            new Vector3(0, 0, 0),
-            new Vector3(0, 0, 1),
-            new Vector3(1, 0, 0),
-            new Vector3(1, 0, 1),
-        };
-
-        triangles = new int[]
-        {
-            0, 1, 2,
-            1, 3, 2,
-        };
-    }
-
-    void UpdateMesh()
-    {
-        mesh.Clear();
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-
-        mesh.RecalculateNormals();
     }
 }
